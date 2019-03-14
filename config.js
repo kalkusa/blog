c5e7a29f8a1c2cd5465aa3ff3a16a160fe74d8b7
Arkadiusz Kałkus <kalkus@10g.pl>
2019-03-14 15:22:43 +0100

    Basic Gatsby config.

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Arkadiusz Kałkus - on software development', // Required
+  author: 'Arkadiusz Kałkus', // Required
+  description: 'Full-stack Web Developer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    // website: 'https://ryanfitzgerald.ca',
+    github: 'https://github.com/kalkusa',
+    twitter: 'https://twitter.com/landeeyo',
+    linkedin: 'https://www.linkedin.com/in/kalkusa',
+  },
+  pathPrefix: '/devblog',
+  //siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+};
