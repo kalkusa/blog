3f4cfbca7d0810af8aa5df81179b567689d826ac
kalkusa <kalkus@10g.pl>
2019-03-14 18:24:06 +0100

    Global-styles fix.

--- src/templates/layout.js
+++ src/templates/layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'prismjs/themes/prism.css';
 
-import '../global-styles';
+import '../global-styles.css';
 import userConfig from '../../config';
 
 import Header from '../components/Header';
